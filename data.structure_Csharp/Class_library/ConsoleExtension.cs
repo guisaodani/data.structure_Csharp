cc15da41bdccc240d61a43ac420505856d6841b0
guisaodani <guisaodani@gmail.com>
2026-01-29 22:40:35 -0500

    a program is integrated for the user interface

---
+++ data.structure_Csharp/Class_library/ConsoleExtension.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_library
+{
+    public class ConsoleExtension
+    {
+        public static int GetInt(string message)
+        {
+            Console.Write(message);
+            var numberString = Console.ReadLine();
+            if (int.TryParse(numberString, out int numberInt))
+            {
+                return numberInt;
+            }
+
+            return 0;
+        }
+
+        public static float GetFloat(string message)
+        {
+            Console.Write(message);
+            var numberString = Console.ReadLine();
+            if (float.TryParse(numberString, out float numberFloat))
+            {
+                return numberFloat;
+            }
+
+            return 0;
+        }
+
+        public static double GetDouble(string message)
+        {
+            Console.Write(message);
+            var numberString = Console.ReadLine();
+            if (double.TryParse(numberString, out double numberDouble))
+            {
+                return numberDouble;
+            }
+
+            return 0;
+        }
+
+        public static decimal GetDecimal(string message)
+        {
+            Console.Write(message);
+            var numberString = Console.ReadLine();
+            if (decimal.TryParse(numberString, out decimal numberDecimal))
+            {
+                return numberDecimal;
+            }
+
+            return 0;
+        }
+
+        public static string? GetString(string message)
+        {
+            Console.Write(message);
+            var text = Console.ReadLine();
+            return text;
+        }
+
+        public static string? GetValidOptions(string message, List<string> options)
+        {
+            Console.Write(message);
+            var answer = Console.ReadLine();
+            if (options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)))
+            {
+                return answer;
+            }
+            return null;
+        }
+    }
+}
--- data.structure_Csharp/DatePOO/Program.cs
+++ data.structure_Csharp/DatePOO/Program.cs
@@ -1,14 +1,33 @@
 ﻿using Class_library;
 
-try
+var answer = String.Empty;
+var options = new List<string> { "s", "n" };
+do
 {
-    var date1 = new Date() { Year = 2028, Mont = 2, Day = 29 };
-    var date2 = new Date() { Year = 1974, Mont = 10, Day = 14 };
+    try
+    {
+        Console.Write("Ingrese un año :");
+        int y = int.Parse(Console.ReadLine()!);
+        Console.Write("Ingrese un mes :");
+        int m = int.Parse(Console.ReadLine()!);
+        Console.Write("Ingrese un dia :");
+        int d = int.Parse(Console.ReadLine()!);
 
-    Console.WriteLine(date1);
-    Console.WriteLine(date2);
+        var date1 = new Date();
+        var date2 = new Date(y, m, d);
+
+        Console.WriteLine(date1);
+        Console.WriteLine(date2);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar S[si], N[no] ? : ", options);
+    }
+    while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
 }
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
+while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
