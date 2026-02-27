78f2db5c931363acb15dc173861308a71e8b771b
guisaodani <guisaodani@gmail.com>
2026-02-26 21:06:57 -0500

    created array whit diferent methods

--- data.structure_Csharp/Arrays.Logic/MyArray.cs
+++ data.structure_Csharp/Arrays.Logic/MyArray.cs
@@ -23,6 +23,72 @@ namespace Arrays.Logic
 
         public int N { get; }  /// propiedad tamaño fisico del arreglo
 
+        public void Add(int number)
+        {
+            if (IsFull)
+            {
+                throw new Exception("El arreglo ya esta lleno");
+            }
+            _array[_top] = number;
+            _top++;
+        }
+
+        public void Insert(int number, int position)
+
+        {
+            if (IsFull)
+            {
+                throw new Exception("El arreglo ya esta lleno");
+            }
+
+            if (position < 0)
+            {
+                position = 0;
+            }
+
+            if (position > _top)
+            {
+                position = _top;
+            }
+
+            for (int i = _top; i > position; i--)
+            {
+                _array[i] = _array[i - 1];
+            }
+
+            _array[position] = number;
+            _top++;
+        }
+
+        public void Remove(int position)
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("El arreglo esta vacio");
+            }
+
+            if (position < 0)
+            {
+                position = 0;
+            }
+
+            if (position > _top)
+            {
+                position = _top;
+            }
+
+            for (int i = position; i < _top - 1; i++)
+            {
+                _array[position] = _array[i + 1];
+            }
+            _top--;
+        }
+
+        public void Fill()
+        {
+            Fill(1, 100);
+        }
+
         public void Fill(int minimun, int maximun)
         {
             Random random = new();
@@ -31,6 +97,162 @@ namespace Arrays.Logic
                 _array[i] = random.Next(minimun, maximun);
             }
             _top = N;
+        }
+
+        public MyArray GetPrimes()
+        {
+            int primescount = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (IsPrime(_array[i]))
+                {
+                    primescount++;
+                }
+            }
+
+            var primesArray = new MyArray(primescount);
+            for (int i = 0; i < _top; i++)
+            {
+                if (IsPrime(_array[i]))
+                {
+                    primesArray.Add(_array[i]);
+                }
+            }
+            return primesArray;
+        }
+
+        public MyArray GetOdds()
+        {
+            var oddCount = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                if (_array[i] % 2 == 0)
+                {
+                    oddCount++;
+                }
+            }
+            var oddArray = new MyArray(oddCount);
+            for (int i = 0; i < _top; i++)
+            {
+                if (_array[i] % 2 == 0)
+                {
+                    oddArray.Add(_array[i]);
+                }
+            }
+            return oddArray;
+        }
+
+        public MyArray GetNonRepeated()
+        {
+            int nonRepeatCount = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                bool isRepeted = false;
+                for (int j = 0; j < _top; j++)
+                {
+                    if (i != j && _array[i] == _array[j])
+                    {
+                        isRepeted = true;
+                        break;
+                    }
+                }
+                if (!isRepeted)
+                {
+                    nonRepeatCount++;
+                }
+            }
+            var nonRepeated = new MyArray(nonRepeatCount);
+            for (int i = 0; i < _top; i++)
+            {
+                bool isRepeted = false;
+                for (int j = 0; j < _top; j++)
+                {
+                    if (i != j && _array[i] == _array[j])
+                    {
+                        isRepeted = true;
+                        break;
+                    }
+                }
+                if (!isRepeted)
+                {
+                    nonRepeated.Add(_array[i]);
+                }
+            }
+
+            return nonRepeated;
+        }
+
+        public MyArray GetMostRepeat()
+        {
+            int[,] matrixCount = new int[_top, 2];
+            int topMatrix = FillMatrixCount(matrixCount);
+            OrderMatrixCount(matrixCount, topMatrix);
+            int repeatedCounter = GetRepeatedNumber(matrixCount, topMatrix);
+            return FillMostRepeated(repeatedCounter, matrixCount);
+        }
+
+        private MyArray FillMostRepeated(int repeatedCounter, int[,] matrixCount)
+        {
+            var mostRepeated = new MyArray(repeatedCounter);
+            for (int i = 0; i < repeatedCounter; i++)
+            {
+                mostRepeated.Add(matrixCount[i, 0]);
+            }
+            return mostRepeated;
+        }
+
+        private int GetRepeatedNumber(int[,] matrixCount, int topMatrix)
+        {
+            int repeatedCounter = 1;
+            for (; repeatedCounter < topMatrix; repeatedCounter++)
+            {
+                if (matrixCount[0, 1] != matrixCount[repeatedCounter, 1])
+                {
+                    break;
+                }
+            }
+            return repeatedCounter;
+        }
+
+        private void OrderMatrixCount(int[,] matrixCount, int topMatrix)
+        {
+            //order by counter
+            for (int i = 0; i < topMatrix - 1; i++)
+            {
+                for (int j = i + 1; j < topMatrix; j++)
+                {
+                    if (matrixCount[i, 1] < matrixCount[j, 1])
+                    {
+                        Change(ref matrixCount[i, 0], ref matrixCount[j, 0]);
+                        Change(ref matrixCount[i, 1], ref matrixCount[j, 1]);
+                    }
+                }
+            }
+        }
+
+        private int FillMatrixCount(int[,] matrixCount)
+        {
+            int topMatrix = 0;
+
+            for (int i = 0; i < _top; i++)
+            {
+                int j = 0;
+                for (; j < topMatrix; j++)
+                {
+                    if (_array[i] == matrixCount[j, 0])
+                    {
+                        matrixCount[j, 1]++;
+                        break;
+                    }
+                }
+                if (j == topMatrix)
+                {
+                    matrixCount[topMatrix, 0] = _array[i];
+                    matrixCount[topMatrix, 1] = 1;
+                    topMatrix++;
+                }
+            }
+            return topMatrix;
         }
 
         public override string ToString()
@@ -56,25 +278,25 @@ namespace Arrays.Logic
 
         public void Sort()
         {
-            Sort(true);
+            Sort(descending: false);
         }
 
         public void Sort(bool descending)
         {
             for (int i = 0; i < _top - 1; i++)
             {
-                for (int j = i + 1; j > _top; j++)
+                for (int j = i + 1; j < _top; j++)
                 {
                     if (descending)
                     {
-                        if (_array[i] > _array[j])
+                        if (_array[i] < _array[j])
                         {
                             Change(ref _array[i], ref _array[j]);
                         }
                     }
                     else
                     {
-                        if (_array[i] < _array[j])
+                        if (_array[i] > _array[j])
                         {
                             Change(ref _array[i], ref _array[j]);
                         }
@@ -83,11 +305,27 @@ namespace Arrays.Logic
             }
         }
 
-        private void Change(ref int a, ref int b)
+        private void Change(ref int a, ref int b)  //parametros por referencia, eso pasa con los tipo de datos primitivos. no con los objetos
         {
             int aux = a;
             a = b;
             b = aux;
+        }
+
+        private bool IsPrime(int n)
+        {
+            if (n == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
--- data.structure_Csharp/Arrays.UI.Console/Program.cs
+++ data.structure_Csharp/Arrays.UI.Console/Program.cs
@@ -1,13 +1,100 @@
 ﻿using Arrays.Logic;
+using System.ComponentModel.Design;
 
-Console.WriteLine("ARRAY");
+try
+{
+    Console.WriteLine("ARRAY");
 
-MyArray array = new(20);
-array.Fill(1, 10);
+    int option;
+    MyArray myArray = new(10);
+    myArray.Fill();
+    do
+    {
+        option = Menu();
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Ingrese numero de elementos: ");
+                var nString = Console.ReadLine();
+                int n = 10;
+                int.TryParse(nString, out n);
+                myArray = new(n);
+                myArray.Fill();
+                break;
 
-Console.WriteLine("Arreglo Desordenado:");
-Console.WriteLine(array);
+            case 2:
+                Console.WriteLine(myArray);
+                break;
 
-array.Sort();
-Console.WriteLine("Arreglo Odenado:");
-Console.WriteLine(array);
+            case 3:
+                Console.WriteLine(myArray.GetOdds());
+                break;
+
+            case 4:
+                Console.WriteLine(myArray.GetPrimes());
+                break;
+
+            case 5:
+                Console.WriteLine(myArray.GetNonRepeated());
+                break;
+
+            case 6:
+                Console.WriteLine(myArray.GetMostRepeat());
+                break;
+
+            default:
+                Console.WriteLine("no jodas!!!!");
+                break;
+        }
+    } while (option != 0);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+int Menu()
+{
+    Console.WriteLine("1. Definir tamaño de arreglo");
+    Console.WriteLine("2. Mostrar arreglo");
+    Console.WriteLine("3. Mostrar numeros Pares");
+    Console.WriteLine("4. Mostrar numeros primos");
+    Console.WriteLine("5. Mostrar numeros que no se repiten");
+    Console.WriteLine("6. Mostrar numeros que mas se repiten");
+    Console.WriteLine("0. salir");
+    bool isValid = false;
+    int option = 0;
+
+    do
+    {
+        Console.Write("Digite su opcion: ");
+        var optionstring = Console.ReadLine();
+        if (!int.TryParse(optionstring, out option))
+        {
+            Console.WriteLine("Opcion incorrecta, debe ingresar solo numeros de 0 a 5");
+            isValid = false;
+        }
+        else
+        {
+            isValid = true;
+        }
+    } while (!isValid || option < 0 || option > 6);
+
+    return option;
+}
+//array.Fill(1, 6);
+
+//Console.WriteLine("Arreglo Desordenado:");
+//Console.WriteLine(array);
+
+//array.Sort(descending: false);
+//Console.WriteLine("Arreglo Odenado Ascendente:");
+//Console.WriteLine(array);
+
+//array.Sort(descending: true);
+//Console.WriteLine("Arreglo Odenado Descendente:");
+//Console.WriteLine(array);
+
+//hacer un metodo que devuelva los numeros primos
+// hacer metodo que devuelva  los que no se repiten
+//hacer metodo que devuelva los que mas se repiten
